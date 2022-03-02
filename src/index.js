3c6750fd71d2c5a987ed8281c239d929f4374b64
Michael Tran <mltrunner@gmail.com>
2022-03-02 10:24:04 -0800

    Completed lab

--- src/index.js
+++ src/index.js
@@ -1,3 +1,24 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.querySelector('#create-task-form');
+  form.addEventListener('submit', handleSubmit);
 });
+
+function handleSubmit(e) {
+  e.preventDefault();
+  const list = document.querySelector('#tasks');
+  const task = document.createElement('li');
+  const description = document.querySelector('#new-task-description').value;
+  task.innerText = description;
+
+  const taskButton = document.createElement('button');
+  taskButton.setAttribute('data-description', description);
+  taskButton.innerText = 'X';
+
+  task.append(taskButton);
+  list.append(task);
+
+  taskButton.addEventListener('click', () => task.remove());
+
+  e.target.reset();
+}
+
